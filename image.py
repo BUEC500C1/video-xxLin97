d2373c98a90a6ff0c86138160e4119a26df758f2
Zhichao_Lu <55032729+xxLin97@users.noreply.github.com>
2020-08-21 02:53:06 -0400

    Update image.py

--- image.py
+++ image.py
@@ -10,12 +10,13 @@ def tweet2image(video, image, tweet):
     if (tw_len > 60):
       text_temp =  text_temp + "\n"
       tw_len = 0
+    txt_temp = txt_temp + " " + tw
   text_temp = text_temp.encode('utf-8')
 
 
  font = ImageFont.load_default()
  filename = "images/" + str(video) + "-" + str(image) + ".png"
- Image = Image.new(mode = "RGB", size = (600,200), color = 'white')
- tweeter = ImageDraw.Draw(Image)
+ tw_image = Image.new(mode = "RGB", size = (600,200), color = 'white')
+ tweeter = ImageDraw.Draw(tw_image)
  tweeter.text((0,50),txt,fill=(0,0,0),spacing=10,align='left')
- Image.save(filename)   
+ tw_image.save(filename)   
